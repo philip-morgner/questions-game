1e87cad2ba38543379a3eabf6db7a2a968f19772
Max <MaxBiermann@gmx.net>
2018-10-26 16:13:23 +0200

    Added functionality for /questions GET

--- server/src/main/java/serverPackage/welcomeHandler.java
+++ server/src/main/java/serverPackage/welcomeHandler.java
@@ -13,8 +13,25 @@ public class welcomeHandler implements HttpHandler{
 	
 	public void handle(HttpExchange he) throws IOException {
 		//status message
-		System.out.println("Incoming query from: "+he.getRemoteAddress().getAddress());
+		String ipAddress = he.getRequestHeaders().getFirst("X-FORWARDED-FOR");  
+		   if (ipAddress == null) {  
+		       ipAddress = he.getRemoteAddress().getAddress().toString();  
+		   }
+		System.out.println("Incoming query from: "+ipAddress);
 		
+		if(he.getRequestMethod().equalsIgnoreCase("GET"))echoGet(he);
+		else {
+			System.out.println("Wrong request method was used, sending error message");
+			Headers head = he.getResponseHeaders();
+			head.set("Content-Type", String.format("application/json; charset=%s", StandardCharsets.UTF_8));
+			String response = "{ \"Error\": \"Http method not supported. Please use GET\" }";
+			byte[] rawResponse = response.getBytes(StandardCharsets.UTF_8);
+			he.sendResponseHeaders(405, rawResponse.length);
+			he.getResponseBody().close();
+		}
+	}
+	
+	private void echoGet(HttpExchange he) throws IOException {
 		//read html-file
 		String htmlfile ="/home/biermann/Dokumente/Etc./qgame/questions-game/server/src/main/java/serverPackage/welcome.html";
 		StringBuilder buildhtml = new StringBuilder();
@@ -24,6 +41,7 @@ public class welcomeHandler implements HttpHandler{
 			while((str=in.readLine())!=null) {
 				buildhtml.append(str);
 			}
+			in.close();
 		} catch(IOException ex) {
 			System.out.println("Reading HTML File failed, sending error message");
 			Headers head = he.getResponseHeaders();
@@ -35,7 +53,7 @@ public class welcomeHandler implements HttpHandler{
 			return;
 		}
 		
-		//prepare response string representing a json object
+		//prepare response string representing a html file
 		String response = buildhtml.toString();
 		
 		//send response with the right headers
@@ -47,7 +65,6 @@ public class welcomeHandler implements HttpHandler{
 		System.out.println("Response was sent");
 		
 		//close stream
-		he.getResponseBody().close();
-		
+		he.getResponseBody().close();		
 	}
 }
